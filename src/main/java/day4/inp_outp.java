52bcbd0aef910e643e6703f99e5c3921bb715502
Haralds Anskins <haralds020202@gmail.com>
2022-08-23 11:40:18 +0300

    Updating I/O task with try/catch

--- src/main/java/day4/inp_outp.java
+++ src/main/java/day4/inp_outp.java
@@ -3,19 +3,26 @@ package day4;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class inp_outp {
     public static void main(String[] args)throws Exception {
         File file = new File(
-                "C:\\Users\\Haralda pc\\IdeaProjects\\day2_AJB\\src\\day4\\text.txt");
+                "C:\\Users\\Haralda pc\\IdeaProjects\\Bootcamp\\src\\main\\java\\day4\\task1.java");
         BufferedReader br = new BufferedReader(new FileReader(file));
+
         String st;
-        String sum = "";
-        while ((st = br.readLine()) != null){
-            sum += st;
+        StringBuilder sum = new StringBuilder();
+
+        try {
+            while ((st = br.readLine()) != null) {
+                sum.append(st);
+            }
+        }catch (IOException e){
+            System.out.println("eror");
         }
-        byte[] b = sum.getBytes();
+        byte[] b = sum.toString().getBytes();
         System.out.print(Arrays.toString(b));
     }
 }
